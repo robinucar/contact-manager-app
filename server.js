0b71433a75cebd7eb25a604afe3a4acf2bdeef66
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-02 22:29:17 +0100

    User model and validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Databases
 connectDB();
 
+//Init middleware
+app.use(express.json({extended: false}));
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the contact keeper API..." })
 );
