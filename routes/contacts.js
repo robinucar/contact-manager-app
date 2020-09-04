b77ed14f841b298244f21142dcbd41fe9db588aa
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-04 22:47:07 +0100

    updated contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -55,8 +55,38 @@ router.post(
 //@route     PUT   api/contact
 //@desc      Update contacts
 //access     Private
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //build a contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not Found..." });
+
+    //Make sure user pwns contact first
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not Authorized... " });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: contactFields,
+      },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+      res.status(500).send("Server Error");
+  }
 });
 
 //@route     DELETE   api/contact
